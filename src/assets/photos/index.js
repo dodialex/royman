43335e94ef4e1fa63b1025bb4f567e3c3dbc54b5
Sturgen <dodi.alexsander@gmail.com>
2021-11-07 22:29:19 +0700

    update revi done

--- src/assets/photos/index.js
+++ src/assets/photos/index.js
@@ -1,31 +1,14 @@
 import Photo1 from './1.jpeg';
 import Photo2 from './2.jpeg';
 import Photo3 from './3.jpeg';
-import Photo4 from './4.jpeg';
 import Photo5 from './5.jpeg';
 import Photo6 from './6.jpeg';
-import Photo7 from './7.jpeg';
-import Photo8 from './8.jpeg';
-import Photo9 from './9.jpeg';
-import Photo10 from './10.jpeg';
 import Photo11 from './11.jpeg';
-import Photo12 from './12.jpeg';
-import Photo13 from './13.jpeg';
-import Photo14 from './14.jpeg';
 import Photo15 from './15.jpeg';
 import Photo16 from './16.jpeg';
-import Photo17 from './17.jpeg';
-import Photo18 from './18.jpeg';
-import Photo19 from './19.jpeg';
 import Photo20 from './20.jpeg';
-import Photo21 from './21.jpeg';
-import Photo22 from './22.jpeg';
-import Photo23 from './23.jpeg';
 import Photo24 from './24.jpeg';
-import Photo25 from './25.jpeg';
 
 export {
-    Photo1, Photo2, Photo3, Photo4, Photo5, Photo6, Photo7, Photo8, Photo9, Photo10,
-    Photo11, Photo12, Photo13, Photo14, Photo15, Photo16, Photo17, Photo18, Photo19, Photo20,
-    Photo21, Photo22, Photo23, Photo24, Photo25
+    Photo1, Photo2, Photo3, Photo5, Photo6, Photo11, Photo15, Photo16, Photo20, Photo24
 };
--- src/components/gambar-gambar/index.js
+++ src/components/gambar-gambar/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import {
-    Photo1, Photo2, Photo3, Photo4, Photo5, Photo6, Photo7, Photo8, Photo9, Photo10,
-    Photo11, Photo12, Photo13, Photo14, Photo15, Photo16, Photo17, Photo18, Photo19, Photo20,
-    Photo21, Photo22, Photo23, Photo24, Photo25
+    Photo1, Photo11, Photo15, Photo16, Photo2, Photo20, Photo24, Photo3, Photo5, Photo6
 } from '../../assets';
 
 
@@ -21,37 +19,13 @@ const GambarGambar = () => {
                     <img src={Photo3} alt='Photo3' />
                 </div>
                 <div>
-                    <img src={Photo4} alt='Photo4' />
-                </div>
-                <div>
                     <img src={Photo5} alt='Photo5' />
                 </div>
                 <div>
                     <img src={Photo6} alt='Photo6' />
                 </div>
                 <div>
-                    <img src={Photo7} alt='Photo7' />
-                </div>
-                <div>
-                    <img src={Photo8} alt='Photo8' />
-                </div>
-                <div>
-                    <img src={Photo9} alt='Photo9' />
-                </div>
-                <div>
-                    <img src={Photo10} alt='Photo10' />
-                </div>
-                <div>
                     <img src={Photo11} alt='Photo11' />
-                </div>
-                <div>
-                    <img src={Photo12} alt='Photo12' />
-                </div>
-                <div>
-                    <img src={Photo13} alt='Photo13' />
-                </div>
-                <div>
-                    <img src={Photo14} alt='Photo14' />
                 </div>
                 <div>
                     <img src={Photo15} alt='Photo15' />
@@ -60,33 +34,11 @@ const GambarGambar = () => {
                     <img src={Photo16} alt='Photo16' />
                 </div>
                 <div>
-                    <img src={Photo17} alt='Photo17' />
-                </div>
-                <div>
-                    <img src={Photo18} alt='Photo18' />
-                </div>
-                <div>
-                    <img src={Photo19} alt='Photo19' />
-                </div>
-                <div>
                     <img src={Photo20} alt='Photo20' />
-                </div>
-                <div>
-                    <img src={Photo21} alt='Photo21' />
-                </div>
-                <div>
-                    <img src={Photo22} alt='Photo22' />
-                </div>
-                <div>
-                    <img src={Photo23} alt='Photo23' />
                 </div>
                 <div>
                     <img src={Photo24} alt='Photo24' />
                 </div>
-                <div>
-                    <img src={Photo25} alt='Photo25' />
-                </div>
-
             </Carousel>
         </div>
     )
